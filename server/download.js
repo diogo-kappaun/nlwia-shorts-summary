982a15748d2ae8e3c3073bb05c77f9594ca05c09
diogo-kappaun <diogohkappaun@gmail.com>
2023-09-17 14:00:12 -0300

    Initial commit

---
+++ server/download.js
@@ -0,0 +1,24 @@
+import ytdl from "ytdl-core";
+import fs from "fs";
+import { error } from "console";
+
+export const download = (videoId) => new Promise((resolve, reject) => {
+  const videoURL = "https://www.youtube.com/shorts/" + videoId;
+  console.log("Realizando o download do vídeo: ", videoId);
+
+  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+    .on("info", (info) => {
+      const seconds = info.formats[0].approxDurationMs / 1000;
+      if (seconds > 60) {
+        throw new Error();
+      }
+    })
+    .on("end", () => {
+      console.log('Download do vídeo finalizado.')
+      resolve();
+    })
+    .on("error", () => {
+      console.log('Não foi possível fazer o download do vídeo. Detalhes do erro: ' + error)
+      reject(error);
+    }).pipe(fs.createWriteStream('./tmp/audio.mp4'))
+});
